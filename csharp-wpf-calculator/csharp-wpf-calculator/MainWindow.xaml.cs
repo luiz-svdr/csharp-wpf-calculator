6746d2bfd03a4ecd2ea68b8bf53ee79e0f763705
Luiz G. Salvador <luizg.salvadoroliveira@gmail.com>
2026-09-20 18:28:24 -0300

    feat: implementa interface da calculadora e lógica inicial de operações

--- csharp-wpf-calculator/csharp-wpf-calculator/MainWindow.xaml.cs
+++ csharp-wpf-calculator/csharp-wpf-calculator/MainWindow.xaml.cs
@@ -19,6 +19,35 @@ namespace csharp_wpf_calculator
         public MainWindow()
         {
             InitializeComponent();
+        }        
+
+        private void NumeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+               string numeroClicado = button.Content.ToString();
+               DisplayOperacao.Text += numeroClicado;
+            }
         }
+        private void OperacaoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                string operacaoClicada = button.Content.ToString();
+                DisplayOperacao.Text += operacaoClicada;
+            }
+        }
+        private void FuncaoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                string funcaoClicada = button.Content.ToString();
+                DisplayOperacao.Text += funcaoClicada;
+            }
+        }
+        
     }
 }
---
+++ csharp-wpf-calculator/csharp-wpf-calculator/Services/CalculadoraService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace csharp_wpf_calculator.Services
+{
+    public class CalculadoraService
+    {
+        public List<string> Numeros { get; private set; } = new List<string>();
+        public List<string> Operadores { get; private set; } = new List<string>();
+        public List<double> NumerosConvertidos { get; private set; } = new List<double>();
+
+        public void ResolverExpressao(string expressao)
+        {
+            string numeroAtual = "";
+
+            foreach (char c in expressao)
+            {
+                if (char.IsDigit(c))
+                {
+                    numeroAtual += c;
+                }
+                else
+                {
+                    Numeros.Add(numeroAtual);
+                    numeroAtual = "";
+                    Operadores.Add(c.ToString());
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(numeroAtual))
+            {
+                Numeros.Add(numeroAtual);
+            }
+
+            foreach (string numero in Numeros)
+            {
+                NumerosConvertidos.Add(double.Parse(numero));
+            }
+        }
+    }
+}
