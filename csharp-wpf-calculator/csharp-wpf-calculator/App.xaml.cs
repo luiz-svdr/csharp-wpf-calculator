642f3a112affe9b4772cdec0c7c5e9fa71a0c070
Luiz G. Salvador <luizg.salvadoroliveira@gmail.com>
2026-09-20 16:13:09 -0300

    chore: initial project setup with .gitignore

---
+++ csharp-wpf-calculator/csharp-wpf-calculator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_wpf_calculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
